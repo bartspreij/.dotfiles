f9123c2b91dcfb7fa2f9388b07947e62f28cb3a0
Bart Spreij <bartspreij@gmail.com>
2024-12-24 23:44:10 +0100

    refactor: switched to blink

---
+++ .config/nvim/lua/plugins/blink.lua
@@ -0,0 +1,37 @@
+return {
+  'saghen/blink.cmp',
+    event = { "LspAttach"},
+  dependencies = 'rafamadriz/friendly-snippets',
+  version = 'v0.*',
+  opts = {
+    -- 'default' for mappings similar to built-in completion
+    -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
+    -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
+    -- see the "default configuration" section below for full documentation on how to define
+    -- your own keymap.
+    keymap = { preset = 'default' },
+
+    appearance = {
+      -- Sets the fallback highlight groups to nvim-cmp's highlight groups
+      -- Useful for when your theme doesn't support blink.cmp
+      -- will be removed in a future release
+      use_nvim_cmp_as_default = true,
+      nerd_font_variant = 'mono'
+    },
+
+    -- default list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, via `opts_extend`
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+      -- optionally disable cmdline completions
+      -- cmdline = {},
+    },
+
+    -- experimental signature help support
+    -- signature = { enabled = true }
+  },
+  -- allows extending the providers array elsewhere in your config
+      -- Adjusts spacing to ensure icons are aligned
+  -- without having to redefine it
+  opts_extend = { "sources.default" }
+}
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,24 +3,11 @@ return {
     dependencies = {
         "williamboman/mason.nvim",
         "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        {
-            "yioneko/nvim-cmp",
-            branch = "perf",
-        },
-        { "L3MON4D3/LuaSnip", dependencies = { "rafamadriz/friendly-snippets" } },
-        "saadparwaiz1/cmp_luasnip",
         "j-hui/fidget.nvim",
         -- Autoformatting
         "stevearc/conform.nvim",
-        -- Lsp extras
-        -- "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
     },
     config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
         require("fidget").setup({})
         require("mason").setup({
             ensure_installed = { "csharpier", "netcoredb", "js-debug-adapter", "delve", "eslint_d", "prettierd", "rustywind" },
@@ -42,15 +29,8 @@ return {
                 "tailwindcss"
             },
         })
-        local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
         local lspconfig = require("lspconfig")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
+        local capabilities = require('blink.cmp').get_lsp_capabilities()
 
         lspconfig.gopls.setup({
             capabilities = capabilities,
@@ -113,82 +93,6 @@ return {
         lspconfig.emmet_language_server.setup({
             capabilities = capabilities,
         })
-
-        -- COMPLETION STUFF
-        -- local cmp_select = { behavior = cmp.SelectBehavior.Insert }
-        -- cmp.setup({
-        --     -- Enable luasnip to handle snippet expansion for nvim-cmp
-        --     snippet = {
-        --         expand = function(args)
-        --             require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-        --         end,
-        --     },
-        --     window = {
-        --         completion = cmp.config.window.bordered(),
-        --         documentation = cmp.config.window.bordered(),
-        --     },
-        --     mapping = cmp.mapping.preset.insert({
-        --         ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        --         ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        --         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        --         ["<C-Space>"] = cmp.mapping.complete(),
-        --     }, { "i", "s" }),
-        --
-        --     sources = cmp.config.sources({
-        --         { name = "nvim_lsp" },
-        --         { name = "luasnip" }, -- For luasnip users.
-        --         { name = "path" },
-        --         { name = "buffer" },
-        --         { name = "terminal" },
-        --     }),
-        -- })
-
-        -- autopairs
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-
-        -- Set up vim-dadbod
-        cmp.setup.filetype({ "sql" }, {
-            sources = {
-                { name = "vim-dadbod-completion" },
-                { name = "buffer" },
-            },
-        })
-
-        -- local ls = require("luasnip")
-        -- ls.config.set_config({
-        --     history = false,
-        --     updateevents = "TextChanged, TextChangedI",
-        -- })
-        --
-        -- vim.keymap.set({ "i", "s" }, "<C-k>", function()
-        --     if ls.expand_or_jumpable() then
-        --         ls.expand_or_jump()
-        --     end
-        -- end, { desc = "Expand or jump forward", silent = true })
-        --
-        -- vim.keymap.set({ "i", "s" }, "<C-j>", function()
-        --     if ls.jumpable(-1) then
-        --         ls.jump(-1)
-        --     end
-        -- end, { desc = "Jump backward", silent = true })
-
-
-        -- DIAGNOSTICS STUFF
-        -- require("lsp_lines").setup()
-        -- vim.keymap.set("", "<leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
-        -- vim.diagnostic.config({
-        --     update_in_insert = true,
-        --     float = {
-        --         focusable = false,
-        --         style = "minimal",
-        --         border = "rounded",
-        --         header = "",
-        --         prefix = "",
-        --     },
-        --     virtual_text = false,
-        --     virtual_lines = { only_current_line = true },
-        -- })
 
         -- FORMATTING STUFF
         require("conform").setup({
